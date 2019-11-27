3af31e376b21a78d0bf961241bba6faaa2187b2c
JKnight54321 <42787771+JKnight54321@users.noreply.github.com>
2019-11-27 12:58:01 -0700

    Finished ParseEmployeeFromString() in class Utility. Function ready to test

---
+++ Programming Challenge SNFC/Definitions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programming_Challenge_SNFC
+{
+    /// <summary>
+    /// A class to store static definitions such as valid states or paycodes
+    /// </summary>
+    static class Definitions
+    {
+        /// <summary>
+        /// A list of valid pay codes used to determine payrate for an employee
+        /// </summary>
+        public static readonly List<char> VALIDPAYCODES = new char[]{'H', 'S'}.ToList();
+
+        /// <summary>
+        /// A list of codes for the states where the company currently has employees
+        /// </summary>
+        public static readonly List<string> VALIDSTATES = new string[]{ "UT", "WY", "NV", "CO", "ID", "AZ", "OR", "WA", "NM", "TX" }.ToList();
+    }
+}
--- Programming Challenge SNFC/Utility.cs
+++ Programming Challenge SNFC/Utility.cs
@@ -9,8 +9,9 @@ namespace Programming_Challenge_SNFC
     /// <summary>
     /// Class containing static utility methods for use throughout the program
     /// </summary>
-    class Utility
+    static class Utility
     {
+        #region ParseEmployeeFromString()
         /// <summary>
         /// ParseEmployeeFromString()
         /// 
@@ -33,11 +34,79 @@ namespace Programming_Challenge_SNFC
                     return null;
                 }
 
-                // Validate Paycode
-                // Validate Payrate
-                // Validate StartDate
-                // Validate TwoWeekHours
+                #region Validate Paycode
+                char paycode;
+                
+                if(false == Char.TryParse(separatedInput[3], out paycode))
+                {
+                    // The paycode was unable to be parsed
+                    return null;
+                }
+
+                if(!(Definitions.VALIDPAYCODES.Contains(paycode)))
+                {
+                    // The paycode is not a valid paycode
+                    return null;
+                }
+                #endregion
+                #region Validate Payrate
+                decimal payrate;
+
+                if(false == Decimal.TryParse(separatedInput[4], out payrate))
+                {
+                    // payrate failed to parse
+                    return null;
+                }
+                #endregion
+                #region Validate StartDate
+                DateTime startDate;
+
+                if(false == DateTime.TryParse(separatedInput[5], out startDate))
+                {
+                    // startDate was invalid and failed to parse
+                    return null;
+                }
+                #endregion
+                #region Validate StateCode
+                if (!(Definitions.VALIDSTATES.Contains(separatedInput[6])))
+                {
+                    // The state code is invalid
+                    return null;
+                }
+                #endregion
+                #region Validate TwoWeekHours
+                int twoWeekHours;
+
+                if(false == Int32.TryParse(separatedInput[7], out twoWeekHours))
+                {
+                    // twoWeekHours was invalid and failed to parse
+                    return null;
+                }
+                #endregion
+
+                Employee returnThis;
+                try
+                {
+                    returnThis = new Employee {
+                        Id = separatedInput[0],
+                        FirstName = separatedInput[1],
+                        LastName = separatedInput[2],
+                        Paycode = paycode,
+                        Payrate = payrate,
+                        StartDate = startDate,
+                        StateCode = separatedInput[6],
+                        TwoWeekHours = twoWeekHours
+                    };
+                }
+                catch(Exception e)
+                {
+                    // An unanticipated error has occurred
+                    return null;
+                }
+
+                return returnThis;
             }
         }
+        #endregion
     }
 }
