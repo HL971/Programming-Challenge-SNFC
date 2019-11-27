134c7a820ef93c0261a066fea5eb60c3d4f54f8d
JKnight54321 <42787771+JKnight54321@users.noreply.github.com>
2019-11-27 12:11:05 -0700

    Add project files.

---
+++ Programming Challenge SNFC/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Programming_Challenge_SNFC.Models
+{
+    /// <summary>
+    /// Model for a given employee
+    /// </summary>
+    class Employee
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public char Paycode { get; set; }
+        public decimal Payrate { get; set; }
+        public DateTime StartDate { get; set; }
+        public string StateCode { get; set; }
+        public int TwoWeekHours { get; set; }
+
+        /// <summary>
+        /// Test Code
+        /// </summary>
+        public string testEmployee()
+        {
+            return Id + " " + FirstName + " " + LastName + " " + Paycode + " " + Payrate + " " + StartDate + " " + StateCode + TwoWeekHours;
+        }
+    }
+}
---
+++ Programming Challenge SNFC/Utility.cs
@@ -0,0 +1,43 @@
+﻿using Programming_Challenge_SNFC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programming_Challenge_SNFC
+{
+    /// <summary>
+    /// Class containing static utility methods for use throughout the program
+    /// </summary>
+    class Utility
+    {
+        /// <summary>
+        /// ParseEmployeeFromString()
+        /// 
+        /// Takes a line from Employees.txt, parses it and transforms it into an employee object
+        /// </summary>
+        /// <param name="input">A single line of text from Employees.txt, or a string of the same format</param>
+        /// <returns>Null if the input string is null, Null if the input string is an invalid format, a valid Employee object otherwise</returns>
+        public static Employee ParseEmployeeFromString(string input)
+        {
+            if(input == null)
+            {
+                return null;
+            }
+            else
+            {
+                List<string> separatedInput = input.Split(new char[] { ',' }).ToList();
+
+                if(separatedInput.Count != 8) // There are either too few, or too many input items. The input is therefore an invalid format
+                {
+                    return null;
+                }
+
+                // Validate Paycode
+                // Validate Payrate
+                // Validate StartDate
+                // Validate TwoWeekHours
+            }
+        }
+    }
+}
